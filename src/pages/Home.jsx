7d2f62e156de4f4e0a527d74aaaee05efdfe76ae
yasingultekin <yasingultekin13@gmail.com>
2022-07-22 23:15:13 +0300

    axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AddTutorial from '../components/AddTutorial'
 import TutorialList from '../components/TutorialList'
-
-
+import axios from 'axios'
 
 
 const Home = () => {
+
+  const [tutorials, setTutorials] = useState();
+
+  const url = 'https://cw-axios-example.herokuapp.com/api/tutorials';
+
+  const getTutorials = async () => {
+    try {
+      const {data} = await axios.get(url);
+      setTutorials(data);
+    } catch (error) {
+        console.log(error);
+    }
+    
+  }
+
+useEffect(() => {
+  getTutorials();
+}, [])
+
+console.log(tutorials);
+
   return (
     <>
     <AddTutorial />
