06dfca4bbaebb4d64e2d6a425b01a847e7c867e5
yasingultekin <yasingultekin13@gmail.com>
2022-07-25 14:46:37 +0300

    css

--- src/components/EditTutorial.jsx
+++ src/components/EditTutorial.jsx
@@ -20,7 +20,31 @@ const EditTutorial = ({ editItem }) => {
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Modal body text goes here.</p>
+      <div className="mb-3">
+    <label htmlFor="title" className="form-label">Title</label>
+    <input 
+    type="text" 
+    className="form-control" 
+    id="title" 
+    placeholder='Enter your title'
+    value={title}
+    onChange={(e) => setTitle(e.target.value)}
+    required
+    />
+  </div>
+
+  <div className="mb-3">
+    <label htmlFor="desc" className="form-label">Description</label>
+    <input 
+    type="text" 
+    className="form-control" 
+    id="desc"
+    placeholder='Enter your description'
+    value={desc}
+    onChange={(e) => setDesc(e.target.value)}
+    required
+    />
+  </div>
       </div>
       <div class="modal-footer">
         <button 
