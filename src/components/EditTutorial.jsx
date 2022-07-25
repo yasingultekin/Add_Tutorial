40c7a1b067b92dac42fe69d079bf2e5a1e279d4c
yasingultekin <yasingultekin13@gmail.com>
2022-07-25 15:40:51 +0300

    editItem

--- src/components/EditTutorial.jsx
+++ src/components/EditTutorial.jsx
@@ -8,7 +8,7 @@ const EditTutorial = ({ editTutorial, editItem }) => {
   
     const handleSave = (e) => {
       e.preventDefault();
-      editTutorial({ id: id, title: title, description: desc });
+      editTutorial({ id, title, desc });
       setTitle("");
       setDesc("");
     };
@@ -19,14 +19,14 @@ const EditTutorial = ({ editTutorial, editItem }) => {
     }, [newTitle, description]);
     
   return (
-    <div class="modal" tabindex="-1" id='edit-modal'>
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div className="modal" tabIndex="-1" id='edit-modal'>
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title">Modal title</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
+      <div className="modal-body">
       <div className="mb-3">
     <label htmlFor="title" className="form-label">Title</label>
     <input 
@@ -53,10 +53,10 @@ const EditTutorial = ({ editTutorial, editItem }) => {
     />
   </div>
       </div>
-      <div class="modal-footer">
+      <div className="modal-footer">
         <button 
         type="button" 
-        class="btn btn-primary"
+        className="btn btn-primary"
         onClick={handleSave} 
         data-bs-dismiss="modal"
         >Save
